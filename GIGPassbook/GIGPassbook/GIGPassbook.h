5e93903c323cf93e85020038675a5dadbf7f3baa
Alejandro Jimenez <alejandro.jimenez@gigigo.com>
2016-06-13 19:54:51 +0200

    - First import

---
+++ GIGPassbook/GIGPassbook/GIGPassbook.h
@@ -0,0 +1,19 @@
+//
+//  GIGPassbook.h
+//  GIGPassbook
+//
+//  Created by Alejandro Jiménez Agudo on 13/6/16.
+//  Copyright © 2016 Gigigo SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GIGPassbook.
+FOUNDATION_EXPORT double GIGPassbookVersionNumber;
+
+//! Project version string for GIGPassbook.
+FOUNDATION_EXPORT const unsigned char GIGPassbookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIGPassbook/PublicHeader.h>
+
+
